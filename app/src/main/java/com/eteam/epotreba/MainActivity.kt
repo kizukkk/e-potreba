f0f17b5abbb87b6c6e880bebdc3c7b6d2340c671
sspaceless <bodnarillia26@gmail.com>
2023-04-11 19:10:49 +0300

    fix: add marker activity starts on profile fragment button click

--- app/src/main/java/com/eteam/epotreba/MainActivity.kt
+++ app/src/main/java/com/eteam/epotreba/MainActivity.kt
@@ -3,48 +3,35 @@ package com.eteam.epotreba
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
 import com.eteam.epotreba.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var bottomNavigationView: BottomNavigationView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val toiletsListFragment = ToiletsListFragment()
         val nearFragment = NearFragment()
         val profileFragment = ProfileFragment()
 
-        loadFragment(nearFragment)
-
-        bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        bottomNavigationView.setOnItemSelectedListener {
+        binding.bottomNavigationView.setOnItemSelectedListener {
             when (it.itemId) {
-                R.id.list -> {
-                    loadFragment(toiletsListFragment)
-                    true
-                }
-                R.id.near -> {
-                    loadFragment(nearFragment)
-                    true
-                }
-                R.id.profile -> {
-                    loadFragment(profileFragment)
-                    true
-                }
-
-                else -> throw IllegalStateException("Invalid menu item id")
+                R.id.list -> setFragment(toiletsListFragment)
+                R.id.near -> setFragment(nearFragment)
+                R.id.profile -> setFragment(profileFragment)
             }
+            true
         }
     }
 
-    private fun loadFragment(fragment: Fragment) {
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.navigationFragmentsContainer, fragment)
-        transaction.commit()
+    private fun setFragment(fragment: Fragment) {
+        supportFragmentManager.commit {
+            replace(R.id.fragmentContainerView, fragment)
+        }
     }
 }
