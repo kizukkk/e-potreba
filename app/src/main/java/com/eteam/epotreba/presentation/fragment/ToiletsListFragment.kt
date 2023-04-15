bd530762fa12b002054791e3647c758a8481f674
Kizukkk <zentro934@gmail.com>
2023-04-15 16:24:33 +0300

    fix: remove necessary import

--- app/src/main/java/com/eteam/epotreba/presentation/fragment/ToiletsListFragment.kt
+++ app/src/main/java/com/eteam/epotreba/presentation/fragment/ToiletsListFragment.kt
@@ -1,10 +1,17 @@
 package com.eteam.epotreba.presentation.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.eteam.epotreba.R
+import com.eteam.epotreba.domain.adapter.MarkerAdapter
+import com.eteam.epotreba.domain.usecase.GetMarkersUseCase
 
 class ToiletsListFragment
     : Fragment(R.layout.fragment_tolets_list) {
-
 
 }
