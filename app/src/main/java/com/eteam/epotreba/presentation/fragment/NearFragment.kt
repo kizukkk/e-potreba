b472068862d98d9394a69eb15313c445bee30928
Kizukkk <zentro934@gmail.com>
2023-04-15 16:12:56 +0300

    updated: moved to a new package

---
+++ app/src/main/java/com/eteam/epotreba/presentation/fragment/NearFragment.kt
@@ -0,0 +1,8 @@
+package com.eteam.epotreba.presentation.fragment
+
+import androidx.fragment.app.Fragment
+import com.eteam.epotreba.R
+
+class NearFragment : Fragment(R.layout.fragment_near) {
+
+}
---
+++ app/src/main/java/com/eteam/epotreba/presentation/fragment/ToiletsListFragment.kt
@@ -0,0 +1,10 @@
+package com.eteam.epotreba.presentation.fragment
+
+import androidx.fragment.app.Fragment
+import com.eteam.epotreba.R
+
+class ToiletsListFragment
+    : Fragment(R.layout.fragment_tolets_list) {
+
+
+}
