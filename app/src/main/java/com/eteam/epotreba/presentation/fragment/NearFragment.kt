f6952d78a3162c53eb65ddadc8549a043785e72d
Kizukkk <zentro934@gmail.com>
2023-04-18 15:19:23 +0300

    update: replaced layout inflate with binding

--- app/src/main/java/com/eteam/epotreba/presentation/fragment/NearFragment.kt
+++ app/src/main/java/com/eteam/epotreba/presentation/fragment/NearFragment.kt
@@ -8,26 +8,28 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.eteam.epotreba.R
+import com.eteam.epotreba.databinding.FragmentNearBinding
 import com.eteam.epotreba.domain.adapter.MarkerAdapter
 import com.eteam.epotreba.domain.usecase.GetMarkersUseCase
 
 class NearFragment : Fragment(R.layout.fragment_near) {
     private val adapter = MarkerAdapter()
     private val items = GetMarkersUseCase().execute()
-
+    lateinit var binding : FragmentNearBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_near, container,false)
+        binding = FragmentNearBinding.inflate(inflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val recycler = view.findViewById<RecyclerView>(R.id.rv_marker_list)
+        val recycler = binding.rvMarkerList
 
         recycler.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
         recycler.adapter = adapter
