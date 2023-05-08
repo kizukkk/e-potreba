3e5026c0c70fe757c86db4c6ae7879cb09dab6d2
Kizukkk <zentro934@gmail.com>
2023-05-08 18:40:19 +0300

    edited: data retrieval from Data Source

--- app/src/main/java/com/eteam/epotreba/data/repository/MarkerRepository.kt
+++ app/src/main/java/com/eteam/epotreba/data/repository/MarkerRepository.kt
@@ -14,7 +14,27 @@ class MarkerRepository() {
         TODO("Implement save data to DB")
     }
 
-    fun getData(): List<MarkerModel>{
-        return GetSomeData().execute()
+    suspend fun getData(): List<MarkerModel>{
+
+        val markerList: MutableList<MarkerModel> = mutableListOf()
+        db.collection("marks")
+            .get()
+            .addOnSuccessListener {
+                for (result in it.documents) {
+                    val position = result.data?.get("position") as? Map<*, *>
+                    val lat = position?.get("latitude").toString().toDouble()
+                    val lng = position?.get("longitude").toString().toDouble()
+
+
+                    val item = MarkerModel(
+                        title = result.data?.get("title").toString(),
+                        about = result.data?.get("about").toString(),
+                        rate = result.data?.get("rate").toString().toDouble() ,
+                        position = LatLng(lat,lng)
+                    )
+                    markerList.add(item)
+                }
+            }.await()
+        return markerList
     }
 }
