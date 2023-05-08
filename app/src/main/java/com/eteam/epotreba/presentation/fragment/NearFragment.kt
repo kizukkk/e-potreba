af16c99971e66803e6d24b982ed9114c4a143456
Kizukkk <zentro934@gmail.com>
2023-05-08 18:48:39 +0300

    added: VM and data Observe

--- app/src/main/java/com/eteam/epotreba/presentation/fragment/NearFragment.kt
+++ app/src/main/java/com/eteam/epotreba/presentation/fragment/NearFragment.kt
@@ -1,39 +1,41 @@
 package com.eteam.epotreba.presentation.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.LifecycleOwner
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.eteam.epotreba.R
 import com.eteam.epotreba.databinding.FragmentNearBinding
 import com.eteam.epotreba.domain.adapter.MarkerAdapter
-import com.eteam.epotreba.domain.usecase.GetMarkersUseCase
+import com.eteam.epotreba.presentation.viewModel.MainViewModel
 
 class NearFragment : Fragment(R.layout.fragment_near) {
+
+    private val viewModel: MainViewModel by activityViewModels()
+
     private val adapter = MarkerAdapter()
-    private val items = GetMarkersUseCase().execute()
-    lateinit var binding : FragmentNearBinding
+    lateinit var binding: FragmentNearBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentNearBinding.inflate(inflater)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
         val recycler = binding.rvMarkerList
 
         recycler.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
         recycler.adapter = adapter
 
-        adapter.submit(items)
+        viewModel.markerList.observe(this as LifecycleOwner) {
+            adapter.submit(it)
+        }
+        return binding.root
     }
 }
