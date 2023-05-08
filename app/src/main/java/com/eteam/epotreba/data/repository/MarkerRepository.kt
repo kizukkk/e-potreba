92d1448b4f04bd8b5044b9c955ee00619043b340
Kizukkk <zentro934@gmail.com>
2023-05-08 18:47:25 +0300

    added: import

--- app/src/main/java/com/eteam/epotreba/data/repository/MarkerRepository.kt
+++ app/src/main/java/com/eteam/epotreba/data/repository/MarkerRepository.kt
@@ -1,10 +1,11 @@
 package com.eteam.epotreba.data.repository
 
 import android.util.Log
-import com.eteam.epotreba.data.database.GetSomeData
 import com.eteam.epotreba.domain.models.MarkerModel
+import com.google.android.gms.maps.model.LatLng
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.tasks.await
 
 class MarkerRepository() {
     private val db = Firebase.firestore
