007c6eafd3ef98876f9e8ab5aed4450170b97118
Kizukkk <zentro934@gmail.com>
2023-05-08 18:36:55 +0300

    added: VM and updating data

--- app/src/main/java/com/eteam/epotreba/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/eteam/epotreba/presentation/activity/MainActivity.kt
@@ -1,19 +1,32 @@
 package com.eteam.epotreba.presentation.activity
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
+import androidx.lifecycle.lifecycleScope
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.eteam.epotreba.R
 import com.eteam.epotreba.databinding.ActivityMainBinding
 import com.eteam.epotreba.presentation.fragment.NearFragment
 import com.eteam.epotreba.presentation.fragment.ProfileFragment
 import com.eteam.epotreba.presentation.fragment.ToiletsListFragment
+import com.eteam.epotreba.presentation.viewModel.MainViewModel
+import kotlinx.coroutines.launch
+
 
 class MainActivity : AppCompatActivity() {
 
+    private val viewModel: MainViewModel by viewModels()
+
+    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
+
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -22,9 +35,15 @@ class MainActivity : AppCompatActivity() {
         val nearFragment = NearFragment()
         val profileFragment = ProfileFragment()
 
+        swipeRefreshLayout = binding.swiperRefreshLayout
+        swipeRefreshLayout.setOnRefreshListener {
+            lifecycleScope.launch {
+                viewModel.update()
+                swipeRefreshLayout.isRefreshing = false
+            }
+        }
+
         //Заплатка по першочерговому виклику фрагменту Near
-        //Через що двічи "роздувався" фрагмент Near: при старті актівіти
-        //Та при виборі пункту "Поруч" в Navbar
         setFragment(toiletsListFragment)
 
         binding.bottomNavigationView.setOnItemSelectedListener {
