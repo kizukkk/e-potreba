0d5f4ca54015914513a03ad90566573a062af698
Kizukkk <zentro934@gmail.com>
2023-05-13 22:53:22 +0300

    added: redirect to detail fragment

--- app/src/main/java/com/eteam/epotreba/presentation/fragment/NearFragment.kt
+++ app/src/main/java/com/eteam/epotreba/presentation/fragment/NearFragment.kt
@@ -13,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.eteam.epotreba.R
 import com.eteam.epotreba.databinding.FragmentNearBinding
 import com.eteam.epotreba.domain.adapter.MarkerAdapter
+import com.eteam.epotreba.domain.models.MarkerModel
 import com.eteam.epotreba.presentation.viewModel.MainViewModel
 
 class NearFragment : Fragment(R.layout.fragment_near) {
@@ -33,9 +34,26 @@ class NearFragment : Fragment(R.layout.fragment_near) {
         recycler.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
         recycler.adapter = adapter
 
+
+
         viewModel.markerList.observe(this as LifecycleOwner) {
             adapter.submit(it)
         }
+
+        adapter.setOnClickListener(object : MarkerAdapter.OnClickListener {
+            override fun onClick(position: Int, model: MarkerModel) {
+
+                viewModel.passMarkerToFragment(model)
+
+                val transaction = activity?.supportFragmentManager?.beginTransaction()
+                if(transaction != null){
+                    transaction.replace(R.id.fragmentContainerView, DetailFragment())
+                    transaction.commit()
+                }
+            }
+
+        })
+
         return binding.root
     }
 }
