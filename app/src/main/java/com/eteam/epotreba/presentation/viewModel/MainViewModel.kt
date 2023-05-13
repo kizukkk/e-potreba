bf6090dd9e8b42184cd0b8d5223e9ddd28ef0a9e
Kizukkk <zentro934@gmail.com>
2023-05-13 22:48:36 +0300

    added: delete to VM

--- app/src/main/java/com/eteam/epotreba/presentation/viewModel/MainViewModel.kt
+++ app/src/main/java/com/eteam/epotreba/presentation/viewModel/MainViewModel.kt
@@ -1,9 +1,9 @@
 package com.eteam.epotreba.presentation.viewModel
 
-import android.annotation.SuppressLint
 import androidx.lifecycle.*
 import com.eteam.epotreba.data.repository.MarkerRepository
 import com.eteam.epotreba.domain.models.MarkerModel
+import com.eteam.epotreba.domain.usecase.DeleteMarkerUseCase
 import com.eteam.epotreba.domain.usecase.GetMarkersUseCase
 import kotlinx.coroutines.launch
 
@@ -12,6 +12,8 @@ class MainViewModel : ViewModel() {
 
     var markerList: MutableLiveData<List<MarkerModel>> =
         MutableLiveData<List<MarkerModel>>(emptyList())
+
+    lateinit var passMarker: MarkerModel
 
     private val getMarkersUseCase by lazy(LazyThreadSafetyMode.NONE) {
         GetMarkersUseCase(repository = MarkerRepository())
