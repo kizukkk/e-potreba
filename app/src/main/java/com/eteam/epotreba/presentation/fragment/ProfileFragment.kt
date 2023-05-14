cd3cf4dcc2980fea4b701d4efc32a403f65ef72e
Kizukkk <zentro934@gmail.com>
2023-05-14 13:45:52 +0300

    added: sign out logic

--- app/src/main/java/com/eteam/epotreba/presentation/fragment/ProfileFragment.kt
+++ app/src/main/java/com/eteam/epotreba/presentation/fragment/ProfileFragment.kt
@@ -9,6 +9,9 @@ import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.eteam.epotreba.R
 import com.eteam.epotreba.presentation.activity.MarkerCreateActivity
+import com.eteam.epotreba.presentation.activity.SignInActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class ProfileFragment : Fragment() {
@@ -18,6 +21,7 @@ class ProfileFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
         val addMarkerButton = view.findViewById<Button>(R.id.addMarkerButton)
+        val signOut = view.findViewById<Button>(R.id.but_sign_out)
 
         addMarkerButton.setOnClickListener {
             activity?.let {
@@ -25,6 +29,15 @@ class ProfileFragment : Fragment() {
                 it.startActivity(intent)
             }
         }
+
+        signOut.setOnClickListener{
+            Firebase.auth.signOut()
+            startActivity(Intent(activity, SignInActivity::class.java))
+            activity?.finish()
+        }
+
+
+
 
         return view
     }
