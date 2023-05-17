18cbd9df3e23dc5e64e3ca7137059cb90303a64b
Kizukkk <zentro934@gmail.com>
2023-05-17 13:04:40 +0300

    added: uid to marker

--- app/src/main/java/com/eteam/epotreba/presentation/activity/MarkerCreateActivity.kt
+++ app/src/main/java/com/eteam/epotreba/presentation/activity/MarkerCreateActivity.kt
@@ -8,9 +8,12 @@ import com.eteam.epotreba.databinding.ActivityMarkerCreateBinding
 import com.eteam.epotreba.domain.models.MarkerModel
 import com.eteam.epotreba.domain.usecase.SaveMarkerUseCase
 import com.google.android.gms.maps.model.LatLng
+import com.google.firebase.auth.FirebaseAuth
 
 
 class MarkerCreateActivity : AppCompatActivity() {
+
+    private val uid = FirebaseAuth.getInstance().uid
 
     private val saveMarkerUseCase by lazy(LazyThreadSafetyMode.NONE){
         SaveMarkerUseCase(repository = MarkerRepository())
@@ -28,7 +31,8 @@ class MarkerCreateActivity : AppCompatActivity() {
             val about: String = binding.aboutField.text.toString();
             val lat: Double = binding.latField.text.toString().toDouble();
             val lng: Double = binding.lngField.text.toString().toDouble();
-            val marker = MarkerModel(title,about, LatLng(lat,lng),0.0)
+            val marker = MarkerModel(
+                title,about, LatLng(lat,lng),0.0,uid.toString())
 
             saveMarkerUseCase.execute(marker)
 
