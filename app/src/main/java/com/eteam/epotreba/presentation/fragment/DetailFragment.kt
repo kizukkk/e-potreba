65d5909f603efce32f21117bff2410042e6f4cb0
Danylo Markovskyi <62791558+kizukkk@users.noreply.github.com>
2023-05-17 16:32:32 +0300

    Dev layout design (#7)
    * added: icon for marker details
    
    * edited: marker details new design
    
    * edited: use new layout
    
    * added: border shape
    
    ---------
    
    Co-authored-by: Kizukkk <zentro934@gmail.com>

--- app/src/main/java/com/eteam/epotreba/presentation/fragment/DetailFragment.kt
+++ app/src/main/java/com/eteam/epotreba/presentation/fragment/DetailFragment.kt
@@ -1,47 +1,51 @@
 package com.eteam.epotreba.presentation.fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.lifecycleScope
 import com.eteam.epotreba.R
 import com.eteam.epotreba.presentation.viewModel.MainViewModel
-import kotlinx.coroutines.launch
 
-class DetailFragment : Fragment(R.layout.fragment_marker_detail) {
+class DetailFragment : Fragment(R.layout.fragment_marker_details) {
 
     private val viewModel: MainViewModel by activityViewModels()
 
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val view = inflater.inflate(R.layout.fragment_marker_detail, container, false)
+        val view = inflater.inflate(R.layout.fragment_marker_details, container, false)
 
-        val title = view.findViewById<TextView>(R.id.title_filed)
-        val about = view.findViewById<TextView>(R.id.about_field)
-        val rate = view.findViewById<TextView>(R.id.score_field)
+        val title = view.findViewById<TextView>(R.id.title_marker)
+        val about = view.findViewById<TextView>(R.id.about_marker)
+        val rate = view.findViewById<TextView>(R.id.score_marker)
+        val price = view.findViewById<FrameLayout>(R.id.price_item)
+//        val monit = view.findViewById<ImageView>(R.id.monetization_icon)
+//        price.visibility = View.GONE
+//        monit.visibility = View.GONE
 
-        val deleteMarkerButton = view.findViewById<Button>(R.id.but_delete)
+        //val deleteMarkerButton = view.findViewById<Button>(R.id.but_delete)
 
         title.text = viewModel.passMarker.title
         about.text = viewModel.passMarker.about
         rate.text = viewModel.passMarker.rate.toString()
 
 
-        deleteMarkerButton.setOnClickListener {
-            lifecycleScope.launch {
-                viewModel.delete(viewModel.passMarker)
-                viewModel.update()
-            }
-        }
+//        deleteMarkerButton.setOnClickListener {
+//            lifecycleScope.launch {
+//                viewModel.delete(viewModel.passMarker)
+//                viewModel.update()
+//            }
+//        }
 
         return view
     }
