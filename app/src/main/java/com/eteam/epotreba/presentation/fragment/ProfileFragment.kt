3ab79a6a47c319c01c23acfa108a6008dcd8975c
Kizukkk <zentro934@gmail.com>
2023-05-17 13:06:33 +0300

    added: current user info

--- app/src/main/java/com/eteam/epotreba/presentation/fragment/ProfileFragment.kt
+++ app/src/main/java/com/eteam/epotreba/presentation/fragment/ProfileFragment.kt
@@ -6,15 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.eteam.epotreba.R
 import com.eteam.epotreba.presentation.activity.MarkerCreateActivity
 import com.eteam.epotreba.presentation.activity.SignInActivity
+import com.eteam.epotreba.presentation.viewModel.MainViewModel
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 
 class ProfileFragment : Fragment() {
+
+    private val viewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -22,6 +27,12 @@ class ProfileFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
         val addMarkerButton = view.findViewById<Button>(R.id.addMarkerButton)
         val signOut = view.findViewById<Button>(R.id.but_sign_out)
+
+        val phoneInfo = view.findViewById<TextView>(R.id.profile_phone)
+        val nameInfo = view.findViewById<TextView>(R.id.name_field)
+
+        phoneInfo.text = viewModel.currentUser?.phoneNumber
+        nameInfo.text = viewModel.currentUser?.displayName
 
         addMarkerButton.setOnClickListener {
             activity?.let {
