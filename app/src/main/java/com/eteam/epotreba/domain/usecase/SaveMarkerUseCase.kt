e05df4ad76d9b95ce3810421ffbeb8ad1a3882b6
Kizukkk <zentro934@gmail.com>
2023-05-18 11:38:36 +0300

    edited: change name save function

--- app/src/main/java/com/eteam/epotreba/domain/usecase/SaveMarkerUseCase.kt
+++ app/src/main/java/com/eteam/epotreba/domain/usecase/SaveMarkerUseCase.kt
@@ -6,7 +6,7 @@ import com.eteam.epotreba.domain.models.MarkerModel
 class SaveMarkerUseCase(val repository: MarkerRepository) {
 
     fun execute(mark: MarkerModel){
-        repository.saveData(marker = mark)
+        repository.save(marker = mark)
     }
 
 }
