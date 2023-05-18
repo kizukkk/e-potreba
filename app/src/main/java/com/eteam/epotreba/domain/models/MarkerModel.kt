d63afafe0bfc0e9afe5167f3f7fc77172b2a8d57
Kizukkk <zentro934@gmail.com>
2023-05-18 11:39:59 +0300

    added: sum rate, votes and price field to model

--- app/src/main/java/com/eteam/epotreba/domain/models/MarkerModel.kt
+++ app/src/main/java/com/eteam/epotreba/domain/models/MarkerModel.kt
@@ -7,11 +7,17 @@ data class MarkerModel(
     val title: String,
     val about: String,
     val position: LatLng,
-    val rate: Double,
-    val userId: String){
-    constructor(title: String,
-                about: String,
-                position: LatLng,
-                rate: Double,
-                userId: String): this("null", title, about, position, rate, userId)
+    var sumRate: Double,
+    val userId: String,
+    var votes: Int,
+    val price: Double
+    ) {
+    constructor(
+        title: String,
+        about: String,
+        position: LatLng,
+        rate: Double,
+        userId: String,
+        price: Double
+    ) : this("null", title, about, position, rate, userId, 0, price)
 }
