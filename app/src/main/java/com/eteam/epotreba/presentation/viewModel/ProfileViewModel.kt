f912e0ccc27d5ab9ab85bcbab33917b7286ce843
Kizukkk <zentro934@gmail.com>
2023-05-23 19:46:44 +0300

    added: implementation of favorites list logic

--- app/src/main/java/com/eteam/epotreba/presentation/viewModel/ProfileViewModel.kt
+++ app/src/main/java/com/eteam/epotreba/presentation/viewModel/ProfileViewModel.kt
@@ -1,32 +1,50 @@
 package com.eteam.epotreba.presentation.viewModel
 
-import android.util.Log
+import android.annotation.SuppressLint
+import android.app.Application
 import androidx.lifecycle.*
+import com.eteam.epotreba.data.repository.MarkerRepository
 import com.eteam.epotreba.domain.models.MarkerModel
+import com.eteam.epotreba.domain.usecase.FavoriteMarkerUseCase
 import com.google.firebase.auth.FirebaseAuth
 
+@SuppressLint("StaticFieldLeak")
+class ProfileViewModel(app: Application) : AndroidViewModel(app) {
 
-class ProfileViewModel : ViewModel() {
+    private val context = getApplication<Application>().applicationContext
 
     var selectedList: Int = 0
 
+    private val getFavoriteMarkerUseCase by lazy(LazyThreadSafetyMode.NONE) {
+        FavoriteMarkerUseCase(MarkerRepository(context))
+    }
 
-    fun getFavoriteMarkers(source: MutableLiveData<List<MarkerModel>>): List<MarkerModel>{
-        return emptyList()
+    suspend fun getFavoriteMarkers(source: MutableLiveData<List<MarkerModel>>): List<MarkerModel>{
+        val uid = FirebaseAuth.getInstance().currentUser!!.uid
+        val listOfId = getFavoriteMarkerUseCase.get(uid)
+        val favList = mutableListOf<MarkerModel>()
+
+        val mark = source.value ?: return emptyList()
+
+        for (item in mark) {
+            if (listOfId.contains(item.id)) {
+                favList.add(item)
+            }
+        }
+
+
+        return favList
     }
 
     fun getOwnMarkers(source: MutableLiveData<List<MarkerModel>>): List<MarkerModel>{
         val owned = mutableListOf<MarkerModel>()
 
-        val mark = source.value
-        if (mark != null) {
-            for (item in mark) {
-                if (item.userId ==  FirebaseAuth.getInstance().currentUser!!.uid) {
-                    owned.add(item)
-                }
+        val mark = source.value ?: return emptyList()
+        for (item in mark) {
+            if (item.userId ==  FirebaseAuth.getInstance().currentUser!!.uid) {
+                owned.add(item)
             }
         }
-        Log.i("Test", owned.toString())
         return owned
     }
 
