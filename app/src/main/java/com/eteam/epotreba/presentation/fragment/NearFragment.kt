0d92c079b819a953a6f1d8ac6426eb2c9cccfb92
Kizukkk <zentro934@gmail.com>
2023-05-28 16:16:33 +0300

    fix: necessary import

--- app/src/main/java/com/eteam/epotreba/presentation/fragment/NearFragment.kt
+++ app/src/main/java/com/eteam/epotreba/presentation/fragment/NearFragment.kt
@@ -1,20 +1,15 @@
 package com.eteam.epotreba.presentation.fragment
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.LifecycleOwner
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.eteam.epotreba.R
-import com.eteam.epotreba.databinding.FragmentNearBinding
 import com.eteam.epotreba.domain.adapter.MarkerAdapter
-import com.eteam.epotreba.domain.models.MarkerModel
-import com.eteam.epotreba.domain.usecase.GetMarkersUseCase
 import com.eteam.epotreba.presentation.viewModel.MainViewModel
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.MarkerOptions
